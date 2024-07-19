3e163b8c9b971cfd1fa4365dbd2e7184a43d4713
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2024-07-19 23:00:00 +0000

    thirdComit header

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -15,6 +15,23 @@ export default function Hero(){
                 <div className="website-factory text-white absolute top-4 hover:-top-56 transition-all duration-9000 text-center 
                 text-5xl ml-10 mt-16 bg-blue-700 w-2/5 h-[500px] p-2 rounded-lg factory-container">
                     <h2 className="tracking-wider bg-blue-900 bg-opacity-90 font-bold  rounded-lg p-4 text-xl lg:text-4xl ">Website factory</h2>
+                    <ul className="text-left text-white text-lg bg-cyan-900 rounded-lg p-4 bg-opacity-90 mt-10">
+                        <li>
+                            Rapid and Frequent Delivery
+                        </li>
+                        <li>
+                            Transparency
+                        </li>
+                        <li>
+                            Continuous Improvement
+                        </li>
+                        <li>
+                            Risk Reduction
+                        </li>
+                        <li>
+                            Increased Customer Satisfaction
+                        </li>
+                    </ul>
                     {/* <span className="text-left text-white text-base bg-gray-900 rounded-lg p-4 bg-opacity-60">Stay competitive, strengthen your business or begin a new enterprise.</span> */}
                 </div>
                 <div className="bg-gray-900 top-44 relative w-screen h-[450px] flex justify-evenly items-start">
